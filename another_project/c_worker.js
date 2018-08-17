0154d50659742f4850c1fde2d7c7267ed1b2194c
brchen777 <eric790620@gmail.com>
2018-08-17 15:11:52 +0800

    Add process exit

--- another_project/c_worker.js
+++ another_project/c_worker.js
@@ -5,6 +5,7 @@
     const { Worker } = require('worker_threads');
     const { env } = process;
     const thread_cnt = 2;
+    let finish_cnt = 0;
 
     console.log(process.argv.slice(2));
     for (let i = 1; i <= thread_cnt; i++) {
@@ -12,7 +13,13 @@
         const wt_worker = new Worker('./wt_worker.js', { workerData: wt_worker_args });
 
         wt_worker.on('message', (msg) => {
-            console.log(msg)
+            console.log(msg);
+
+            ++finish_cnt;
+            if (finish_cnt === thread_cnt) {
+                console.log(`Process ${i} exiting.`);
+                process.exit(0);
+            }
         });
     }
 })();
